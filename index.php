12f48fa0983a49562f28dda580b23ac6fd257674
mateus <mateusfcardozo245@gmail.com>
2023-09-01 21:53:12 -0300

    .

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Exercícios PHP</title>
+    <title>Test PHP</title>
 </head>
 <body>
     <header>
-        <h1>Exercícios PHP</h1>
+        <h1>Test PHP</h1>
     </header>
     <nav>
         <ul>
@@ -13,12 +13,13 @@
             <li><a href="sobre.php">Sobre</a></li>
             <li><a href="contato.php">Contato</a></li> -->
          <li><a href="exercicios/"> exercícios</a></li> 
-         <li><a href="sistema_solar/"> sistema_solar</a></li> 
+         <li><a href="sistema_solar/"> sistema solar</a></li> 
+         <li><a href="fazendo_testes/"> fazendo testes</a></li> 
 
         </ul>
     </nav>
     <footer>
-        <p>&copy; <?php echo date("Y"); ?> Exercícios PHP. Todos os direitos reservados.</p>
+        <p>&copy; <?php echo date("Y"); ?> Test PHP. Todos os direitos reservados.</p>
     </footer>
 </body>
 </html>
